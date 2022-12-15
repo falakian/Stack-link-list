4361a9814ab083ce3efbae081be1db5daf9bdd9e
falakian <ali.falakian20@gamil.com>
2022-12-15 14:07:11 +0330

    Write function comments

--- Stack.cpp
+++ Stack.cpp
@@ -14,27 +14,31 @@
 
 using namespace std;
 
-
+// Constructor of the stack class in which we null the head and tail values
 Stack::Stack()
 {
     this->head= nullptr;
     this->tail= nullptr;
 }
-
+// Destructive stack class in which we remove dynamic memory
 Stack::~Stack()
 {
-    delete this->head;
-    this->head= nullptr;
-    delete this->tail;
-    this->head= nullptr;
+    while(!this->isEmpty()) // It clears all members until the stack is empty
+    {
+        StackNode *tmp = this->head;
+        head=head->next;
+        delete tmp;
+        tmp=nullptr;
+    }
+    this->tail= nullptr;
 }
-
+// Add data to the end of the stack class
 void Stack::push(int x)
 {
     StackNode *tmp = new StackNode;
     tmp->next = nullptr;
     tmp->data = x;
-    if(this->isEmpty())
+    if(this->isEmpty())  // If the stack was empty
         this->head=this->tail=tmp;
     else
     {
@@ -43,17 +47,17 @@ void Stack::push(int x)
     }
     return;
 }
-
+// The function to remove the last element of the stack and return its value
 int Stack::pop()
 {
-    if(isEmpty())
+    if(isEmpty()) // If the stack was empty
     {
         cout<<"Stack is empty"<<endl;
         return -1;
     }
     else
     {
-        int top = this->peek();
+        int top = this->peek(); // The value of the last element of the stack
         if(this->head!=this->tail)
         {
             StackNode *tmp = this->head;
@@ -63,7 +67,7 @@ int Stack::pop()
             delete this->tail;
             this->tail = tmp;
         }
-        else
+        else // If we had only one element in the stack
         {
             delete this->head;
             this->head =nullptr;
@@ -72,12 +76,12 @@ int Stack::pop()
         return top;
     }
 }
-
+// The value of the last element of the stack
 int Stack::peek() const
 {
     return tail->data;
 }
-
+// Check if the stack is empty
 bool Stack::isEmpty() const
 {
     if (this->head==nullptr )
